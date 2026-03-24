c54584c510bd00378df93eb428a0451ab6928fd6
Ricardo Bastardo <bastvai@gmail.com>
2026-03-24 11:45:44 -0400

    Estructura de archivos y arquitectura por capas

---
+++ app/models/post.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+from typing import List, TYPE_CHECKING, Optional
+from sqlalchemy import Column, Integer, String, Table, Text, DateTime, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.core.db import Base
+from datetime import datetime
+
+# Pare eliminar el error de Pylance
+if TYPE_CHECKING:
+    from .author import AuthorORM
+    from .tag import TagORM
+
+post_tags = Table(
+    "post_tags",
+    Base.metadata,
+    Column("post_id", ForeignKey(
+        "posts.id", ondelete="CASCADE"), primary_key=True),
+    Column("tag_id", ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True)
+)
+
+
+
+class PostORM(Base):
+    __tablename__ = "posts"
+    __table_args__ = (UniqueConstraint("title", name="unique_post_title"),)
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False, index=True)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow)
+
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("authors.id"))
+    author: Mapped[Optional["AuthorORM"]] = relationship(
+        back_populates="posts")
+
+    tags: Mapped[List["TagORM"]] = relationship(
+        secondary=post_tags,
+        back_populates="posts",
+        lazy="selectin",
+        passive_deletes=True
+    )
